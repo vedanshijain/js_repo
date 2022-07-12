300cf3c7d5eeaed97145d1cedf41103f8ddb30bb
vedanshijain <vedanshi.jain@indiamart.com>
2022-07-12 10:56:26 +0530

    c

---
+++ 2.undefined/undefined.js
@@ -0,0 +1,14 @@
+var a;
+console.log(a)
+
+
+if (a===undefined){
+    console.log("a is undefined")
+}
+
+else{
+    console.log("a is defined") 
+}
+
+var a=undefined
+console.log(a, "hello",typeof a)
